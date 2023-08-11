bb1738bb8fa737ca2b8c596c7df838f8563f8c7e
BlackWolf8100 <142004118+BlackWolf8100@users.noreply.github.com>
2023-08-11 14:11:51 +0300

    Update myrequsts.py

--- myrequsts.py
+++ myrequsts.py
@@ -1,11 +1,10 @@
 import requests
-# url = 'https://ikscs.in.ua/'
+# url = 'https://Вписуємо свою силку'
 url = 'https://4.ident.me/'
 headers = {
-    'User-Agent' : 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36'
+    'User-Agent' : 'Вписуємо свій User-Agent'
 }
-# https_proxy = 'https://138.199.48.1:8443'
-socks_proxy = 'socks5://127.0.0.1:9150'
+socks_proxy = 'socks5://Вписуємо свій proxy'
 
 proxies = { 
             "https" : socks_proxy
